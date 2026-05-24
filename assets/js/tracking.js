b2f82daef96d70e2cb96b3198fdfe5238a7c1de3
methode-tee <tiana_pondja@outlook.fr>
2026-05-24 14:33:21 +0200

    Create tracking.js

---
+++ assets/js/tracking.js
@@ -0,0 +1,15 @@
+export function buildChart(){
+
+const ctx = document.getElementById('vitalityChart');
+
+new Chart(ctx,{
+type:'line',
+data:{
+labels:['S1','S2','S3','S4','S5','S6'],
+datasets:[{
+label:'Vitalité',
+data:[58,64,69,74,81,87]
+}]
+}
+});
+}
