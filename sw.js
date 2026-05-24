f3d5ab971f81e528b512775ab26c9cb08b4a6a09
methode-tee <tiana_pondja@outlook.fr>
2026-05-24 15:14:03 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='methode-tee-v4-stable-cache';
-const ASSETS=['./','./index.html','./admin.html','./style.css','./app.js','./landing.html'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
